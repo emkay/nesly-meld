57ccb8bf0cedc49124f80cf46f5836b37c2f75dc
Michael Matuzak <mmatuzak@gmail.com>
2014-07-05 08:33:20 -0700

    simplify, use nesly-split

--- index.js
+++ index.js
@@ -1,17 +1,21 @@
-var ns = require('nesly-split');
 var fs = require('fs');
+var ns = require('nesly-split');
 
-var Buffer = require('buffer').Buffer;
+module.exports = function (file, newChr, cb) {
 
-
-fs.open(__dirname + '/smb.nes', 'r', function (err, fd) {
-    var buffer = new Buffer(8192);
-
-    if (!err) {
-        fs.read(fd, buffer, 0, 8192, 1024 * 8, function (err, bytesRead, buf) {
-            console.log(err);
-            console.log(bytesRead);
-            console.log(buffer);
-        });
+    if (!file || !newChr) {
+        cb(new Error("`file` and `newChr` are required"));
     }
-});
+
+    ns(file, function (err, data) {
+        var rom = data.rom;
+        var chr = data.chr;
+        var size = data.chrSize;
+        var start = data.chrStart;
+
+        var oldChr = rom.slice(start, size);
+        oldChr = newChr;
+
+        cb(null, rom);
+    });
+};
